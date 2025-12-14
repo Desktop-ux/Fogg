c639a648eefab14e26cc604d96f922e61484c2f5
Ayaan <desktop.ux@gmail.com>
2025-12-14 16:09:28 +0530

    Faq head done

---
+++ src/Components/FaqItem/FaqItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './faqitem.css'
+
+const FaqItem = () => {
+  return (
+    <div>FaqItem</div>
+  )
+}
+
+export default FaqItem
--- src/Components/Faqs/Faqs.jsx
+++ src/Components/Faqs/Faqs.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 import './faqs.css'
 import gsap from 'gsap'
 
+import logo_white from '../../Assets/images/logo_white.png'
+
 const Faqs = () => {
   return (
-    <div>
-        <h1>FAQS</h1>
+    <div className='faq'>
+      <div className="logo_faq">
+        <img src={logo_white} alt="" />
+
+      </div>
+      <div class="lux-line"></div>
+
+      <p className='faq_head'>Frequently Asked Questions</p>
+      <h2 className='faq_main' >Everything you need to know about <br />
+        <span>Elite collection</span></h2>
+      <p className='faq_text'>Find answers to common questions about our Elite fragnancies, shipping, returns, and more.</p>
     </div>
   )
 }
